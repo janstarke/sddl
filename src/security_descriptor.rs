a4d6275ffd4eff5408e2c19f3389bd71ac4764b7
Jan Starke <jan.starke@posteo.de>
2024-09-30 17:13:53 +0200

    add remaining fields

--- src/security_descriptor.rs
+++ src/security_descriptor.rs
@@ -2,10 +2,18 @@ use binrw::binrw;
 
 use crate::ControlFlags;
 
+/// <https://github.com/microsoft/referencesource/blob/master/mscorlib/system/security/accesscontrol/securitydescriptor.cs>
 #[binrw]
 pub struct SecurityDescriptor {
     flags: ControlFlags,
 
     #[br(ignore)]
     _reserved1: u8,
+
+    revision: u8,
+
+    owner: u32,
+    group: u32,
+    sacl: u32,
+    dacl: u32
 }
