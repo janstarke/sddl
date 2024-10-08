15b25182f6aaa346e91ff1be77b86d397b51cd35
Jan Starke <jan.starke@posteo.de>
2024-10-08 13:58:42 +0200

    add offset

--- src/security_descriptor.rs
+++ src/security_descriptor.rs
@@ -1,13 +1,15 @@
-use binrw::binrw;
+use binrw::{binread, BinRead, FilePtr};
 use getset::Getters;
 
-use crate::ControlFlags;
+use crate::{Acl, ControlFlags, Sid};
 
 /// <https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/2918391b-75b9-4eeb-83f0-7fdc04a5c6c9>
-#[binrw]
+#[binread]
 #[derive(Eq, PartialEq, Getters)]
 #[getset(get = "pub")]
 pub struct SecurityDescriptor {
+    sd_offset: Offset,
+
     #[br(assert(revision == 1))]
     #[bw(assert(*revision == 1))]
     revision: u8,
@@ -17,15 +19,30 @@ pub struct SecurityDescriptor {
 
     flags: ControlFlags,
 
-    #[brw(big)]
-    owner_offset: u32,
+    #[brw(big, offset=sd_offset.0)]
+    owner_ref: FilePtr<u32, Sid>,
 
-    #[brw(big)]
-    group_offset: u32,
+    #[brw(big, offset=sd_offset.0)]
+    group_ref: FilePtr<u32, Sid>,
 
-    #[brw(big)]
-    sacl_offset: u32,
+    #[brw(big, offset=sd_offset.0)]
+    sacl_ref: FilePtr<u32, Acl>,
 
-    #[brw(big)]
-    dacl_offset: u32,
+    #[brw(big, offset=sd_offset.0)]
+    dacl_ref: FilePtr<u32, Acl>,
+}
+
+#[derive(Eq, PartialEq, Copy, Clone)]
+struct Offset(u64);
+impl BinRead for Offset {
+    type Args<'a> = ();
+
+    fn read_options<R: std::io::Read + std::io::Seek>(
+        reader: &mut R,
+        _endian: binrw::Endian,
+        _args: Self::Args<'_>,
+    ) -> binrw::BinResult<Self> {
+        let offset = reader.stream_position()?;
+        Ok(Self(offset))
+    }
 }
