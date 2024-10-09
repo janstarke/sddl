9e331da409e6cc665ed00b3e4b89df648b1fea47
Jan Starke <jan.starke@posteo.de>
2024-10-09 21:09:43 +0200

    update sddl.h

--- misc/sddl.h
+++ misc/sddl.h
@@ -1,7 +1,6 @@
- 
 /*++
 
-Copyright (c) 1997-1998  Microsoft Corporation
+Copyright (c) Microsoft Corporation. All rights reserved.
 
 Module Name:
 
@@ -29,29 +28,12 @@ Revision History:
 #include <minwindef.h>
 #endif
 
-/* APISET_NAME: api-ms-win-security-sddl-l1 */
-
-#if !defined(RC_INVOKED)
-
-#ifndef _APISET_SDDL_VER
-#ifdef _APISET_TARGET_VERSION
-#if _APISET_TARGET_VERSION >= _APISET_TARGET_VERSION_WIN7
-#define _APISET_SDDL_VER 0x0100
-#endif
-#endif
-#endif
-
-#endif // !defined(RC_INVOKED)
-
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#pragma region Desktop Family or OneCore Family
-
-#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
+#pragma region Application Family or OneCore Family
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP | WINAPI_PARTITION_SYSTEM)
 
 //
 // SDDL Version information
@@ -94,7 +76,7 @@ extern "C" {
 #define SDDL_SCOPED_POLICY_ID               TEXT("SP")  // Scoped policy
 #define SDDL_CALLBACK_AUDIT                 TEXT("XU")  // Callback audit
 #define SDDL_CALLBACK_OBJECT_ACCESS_ALLOWED TEXT("ZA")  // Callback object access allowed
-
+#define SDDL_ACCESS_FILTER                  TEXT("FL")  // Access Filter
 
 //
 // SDDL Resource attribute ace data types
@@ -107,7 +89,6 @@ extern "C" {
 #define SDDL_BLOB                           TEXT("TX")   // Octet String
 #define SDDL_BOOLEAN                        TEXT("TB")   // Boolean
 
-
 //
 // SDDL Ace flags
 //
@@ -116,9 +97,10 @@ extern "C" {
 #define SDDL_NO_PROPAGATE                   TEXT("NP")  // Inherit no propagate
 #define SDDL_INHERIT_ONLY                   TEXT("IO")  // Inherit only
 #define SDDL_INHERITED                      TEXT("ID")  // Inherited
+#define SDDL_CRITICAL                       TEXT("CR")  // Critical
+#define SDDL_TRUST_PROTECTED_FILTER         TEXT("TP")  // Trust Protected Filter
 #define SDDL_AUDIT_SUCCESS                  TEXT("SA")  // Audit success
 #define SDDL_AUDIT_FAILURE                  TEXT("FA")  // Audit failure
-
 
 //
 // SDDL Rights
@@ -151,7 +133,6 @@ extern "C" {
 #define SDDL_NO_WRITE_UP                    TEXT("NW")
 #define SDDL_NO_READ_UP                     TEXT("NR")
 #define SDDL_NO_EXECUTE_UP                  TEXT("NX")
-
 
 //
 // SDDL User alias max size
@@ -214,12 +195,12 @@ extern "C" {
 #define SDDL_ALL_APP_PACKAGES               TEXT("AC")      // All applications running in an app package context
 #define SDDL_RDS_REMOTE_ACCESS_SERVERS      TEXT("RA")      // Servers in this group enable users of RemoteApp programs and personal virtual desktops access to these resources.
 #define SDDL_RDS_ENDPOINT_SERVERS           TEXT("ES")      // Servers in this group run virtual machines and host sessions where users RemoteApp programs and personal virtual desktops run.
-#define SDDL_RDS_MANAGEMENT_SERVERS         TEXT("MS")      // Servers in this group can perform routine administrative actions on servers running Remote Desktop Services. 
+#define SDDL_RDS_MANAGEMENT_SERVERS         TEXT("MS")      // Servers in this group can perform routine administrative actions on servers running Remote Desktop Services.
 #define SDDL_USER_MODE_DRIVERS              TEXT("UD")      // UserMode driver
-#define SDDL_HYPER_V_ADMINS                 TEXT("HA")      // Members of this group have complete and unrestricted access to all features of Hyper-V. 
-#define SDDL_CLONEABLE_CONTROLLERS          TEXT("CN")      // Members of this group that are domain controllers may be cloned. 
-#define SDDL_ACCESS_CONTROL_ASSISTANCE_OPS  TEXT("AA")      // Members of this group can remotely query authorization attributes and permissions for resources on this computer. 
-#define SDDL_REMOTE_MANAGEMENT_USERS        TEXT("RM")      // Members of this group can access WMI resources over management protocols (such as WS-Management via the Windows Remote Management service). This applies only to WMI namespaces that grant access to the user. 
+#define SDDL_HYPER_V_ADMINS                 TEXT("HA")      // Members of this group have complete and unrestricted access to all features of Hyper-V.
+#define SDDL_CLONEABLE_CONTROLLERS          TEXT("CN")      // Members of this group that are domain controllers may be cloned.
+#define SDDL_ACCESS_CONTROL_ASSISTANCE_OPS  TEXT("AA")      // Members of this group can remotely query authorization attributes and permissions for resources on this computer.
+#define SDDL_REMOTE_MANAGEMENT_USERS        TEXT("RM")      // Members of this group can access WMI resources over management protocols (such as WS-Management via the Windows Remote Management service). This applies only to WMI namespaces that grant access to the user.
 #define SDDL_AUTHORITY_ASSERTED             TEXT("AS")      // Authentication Authority Asserted
 #define SDDL_SERVICE_ASSERTED               TEXT("SS")      // Authentication Service Asserted
 #define SDDL_PROTECTED_USERS                TEXT("AP")      // Members of this group are afforded additional protections against authentication security threats.
@@ -243,44 +224,41 @@ extern "C" {
 //
 // SDDL Seperators - character version
 //
-#define SDDL_SEPERATORC                     TEXT(';')
-#define SDDL_DELIMINATORC                   TEXT(':')
-#define SDDL_ACE_BEGINC                     TEXT('(')
-#define SDDL_ACE_ENDC                       TEXT(')')
-#define SDDL_SPACEC                              TEXT(' ')
-#define SDDL_ACE_COND_BEGINC           TEXT('(')
-#define SDDL_ACE_COND_ENDC           TEXT(')')
-#define SDDL_ACE_COND_STRING_BEGINC           TEXT('"')
-#define SDDL_ACE_COND_STRING_ENDC           TEXT('"')
+#define SDDL_SEPERATORC                           TEXT(';')
+#define SDDL_DELIMINATORC                         TEXT(':')
+#define SDDL_ACE_BEGINC                           TEXT('(')
+#define SDDL_ACE_ENDC                             TEXT(')')
+#define SDDL_SPACEC                               TEXT(' ')
+#define SDDL_ACE_COND_BEGINC                      TEXT('(')
+#define SDDL_ACE_COND_ENDC                        TEXT(')')
+#define SDDL_ACE_COND_STRING_BEGINC               TEXT('"')
+#define SDDL_ACE_COND_STRING_ENDC                 TEXT('"')
 #define SDDL_ACE_COND_COMPOSITEVALUE_BEGINC       TEXT('{')
-#define SDDL_ACE_COND_COMPOSITEVALUE_ENDC           TEXT('}')
-#define SDDL_ACE_COND_COMPOSITEVALUE_SEPERATORC           TEXT(',')
-#define SDDL_ACE_COND_BLOB_PREFIXC          TEXT('#')
-#define SDDL_ACE_COND_SID_BEGINC           TEXT('(')
-#define SDDL_ACE_COND_SID_ENDC           TEXT(')')
+#define SDDL_ACE_COND_COMPOSITEVALUE_ENDC         TEXT('}')
+#define SDDL_ACE_COND_COMPOSITEVALUE_SEPERATORC   TEXT(',')
+#define SDDL_ACE_COND_BLOB_PREFIXC                TEXT('#')
+#define SDDL_ACE_COND_SID_BEGINC                  TEXT('(')
+#define SDDL_ACE_COND_SID_ENDC                    TEXT(')')
 
 //
 // SDDL Seperators - string version
 //
-#define SDDL_SEPERATOR                     TEXT(";")
-#define SDDL_DELIMINATOR                   TEXT(":")
-#define SDDL_ACE_BEGIN                     TEXT("(")
-#define SDDL_ACE_END                       TEXT(")")
-#define SDDL_ACE_COND_BEGIN           TEXT("(")
-#define SDDL_ACE_COND_END           TEXT(")")
-#define SDDL_SPACE                              TEXT(" ")
-#define SDDL_ACE_COND_BLOB_PREFIX          TEXT("#")
-#define SDDL_ACE_COND_SID_PREFIX          TEXT("SID")
-#define SDDL_ACE_COND_ATTRIBUTE_PREFIX          TEXT("@")
-#define SDDL_ACE_COND_USER_ATTRIBUTE_PREFIX          TEXT("@USER.")
-#define SDDL_ACE_COND_RESOURCE_ATTRIBUTE_PREFIX          TEXT("@RESOURCE.")
-#define SDDL_ACE_COND_DEVICE_ATTRIBUTE_PREFIX          TEXT("@DEVICE.")
-
-
-
+#define SDDL_SEPERATOR                            TEXT(";")
+#define SDDL_DELIMINATOR                          TEXT(":")
+#define SDDL_ACE_BEGIN                            TEXT("(")
+#define SDDL_ACE_END                              TEXT(")")
+#define SDDL_ACE_COND_BEGIN                       TEXT("(")
+#define SDDL_ACE_COND_END                         TEXT(")")
+#define SDDL_SPACE                                TEXT(" ")
+#define SDDL_ACE_COND_BLOB_PREFIX                 TEXT("#")
+#define SDDL_ACE_COND_SID_PREFIX                  TEXT("SID")
+#define SDDL_ACE_COND_ATTRIBUTE_PREFIX            TEXT("@")
+#define SDDL_ACE_COND_USER_ATTRIBUTE_PREFIX       TEXT("@USER.")
+#define SDDL_ACE_COND_RESOURCE_ATTRIBUTE_PREFIX   TEXT("@RESOURCE.")
+#define SDDL_ACE_COND_DEVICE_ATTRIBUTE_PREFIX     TEXT("@DEVICE.")
+#define SDDL_ACE_COND_TOKEN_ATTRIBUTE_PREFIX      TEXT("@TOKEN.")
 
 #if !defined(_NTDDK_)
-
 
 #if (_WIN32_WINNT >= 0x0500)
 _Success_(return != FALSE)
@@ -296,9 +274,8 @@ BOOL
 WINAPI
 ConvertSidToStringSidW(
     _In_ PSID Sid,
-    _Outptr_ LPWSTR * StringSid
+    _Outptr_ LPWSTR* StringSid
     );
-
 #ifdef UNICODE
 #define ConvertSidToStringSid  ConvertSidToStringSidW
 #else
@@ -318,9 +295,8 @@ BOOL
 WINAPI
 ConvertStringSidToSidW(
     _In_ LPCWSTR StringSid,
-    _Outptr_ PSID * Sid
+    _Outptr_ PSID* Sid
     );
-
 #ifdef UNICODE
 #define ConvertStringSidToSid  ConvertStringSidToSidW
 #else
@@ -343,10 +319,9 @@ WINAPI
 ConvertStringSecurityDescriptorToSecurityDescriptorW(
     _In_ LPCWSTR StringSecurityDescriptor,
     _In_ DWORD StringSDRevision,
-    _Outptr_ PSECURITY_DESCRIPTOR * SecurityDescriptor,
+    _Outptr_ PSECURITY_DESCRIPTOR* SecurityDescriptor,
     _Out_opt_ PULONG SecurityDescriptorSize
     );
-
 #ifdef UNICODE
 #define ConvertStringSecurityDescriptorToSecurityDescriptor  ConvertStringSecurityDescriptorToSecurityDescriptorW
 #else
@@ -371,30 +346,24 @@ ConvertSecurityDescriptorToStringSecurityDescriptorW(
     _In_ PSECURITY_DESCRIPTOR SecurityDescriptor,
     _In_ DWORD RequestedStringSDRevision,
     _In_ SECURITY_INFORMATION SecurityInformation,
-    _Outptr_ LPWSTR * StringSecurityDescriptor,
+    _Outptr_ LPWSTR* StringSecurityDescriptor,
     _Out_opt_ PULONG StringSecurityDescriptorLen
     );
-
 #ifdef UNICODE
 #define ConvertSecurityDescriptorToStringSecurityDescriptor  ConvertSecurityDescriptorToStringSecurityDescriptorW
 #else
 #define ConvertSecurityDescriptorToStringSecurityDescriptor  ConvertSecurityDescriptorToStringSecurityDescriptorA
 #endif // !UNICODE
 
+#endif // WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP | WINAPI_PARTITION_SYSTEM)
+#pragma endregion
 
 #endif /* _WIN32_WINNT >=  0x0500 */
 
 #endif /* !defined(_NTDDK_) */
-
-
-#endif // WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
-#pragma endregion
-
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif  // endif __SDDL_H__
-
-
