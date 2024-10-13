c4369d2d2b1ca795e39a5c7cbb80d0353949822a
Jan Starke <jan.starke@posteo.de>
2024-10-13 11:48:07 +0200

    simplify doc test

--- src/sid/mod.rs
+++ src/sid/mod.rs
@@ -36,7 +36,7 @@ pub const MAX_SUB_AUTHORITIES: u8 = 15;
 pub struct Sid {
     revision: u8,
 
-    #[br(assert(sub_authority_count <= MAX_SUB_AUTHORITIES))]
+    #[br(dbg, assert(sub_authority_count <= MAX_SUB_AUTHORITIES))]
     #[bw(assert(*sub_authority_count <= MAX_SUB_AUTHORITIES))]
     sub_authority_count: u8,
 
