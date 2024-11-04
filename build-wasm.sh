368113f98e3a14fb526b5e914ae5329056bf76ce
Jan Starke <jan.starke@posteo.de>
2024-11-04 16:00:26 +0100

    sync version

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIST_DIR="${SCRIPT_DIR}/target/dist/sddl-wasm"
 
 cargo install wasm-pack
 
-cargo build --release && (cd sddl-wasm; wasm-pack build --release --target bundler --out-dir ${SCRIPT_DIR}/target/dist/sddl-wasm)
+cargo build --release && \
+    (cd sddl-wasm; wasm-pack build --release --target web --out-dir "${DIST_DIR}") && \
+    echo "created wasm distributable in '${DIST_DIR}'"
