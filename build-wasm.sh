c40c5c39b4894359ce5d68c568ee6321e9907f8c
Jan Starke <jan.starke@bdosecurity.de>
2024-11-05 17:44:31 +0100

    change sddl4web path

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DIST_DIR="${SCRIPT_DIR}/target/dist/sddl4web"
+DIST_DIR="${SCRIPT_DIR}/docs/sddl4web"
 
 cargo install wasm-pack
 
