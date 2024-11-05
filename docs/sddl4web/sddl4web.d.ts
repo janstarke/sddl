ba22476255e43292c3874bc087f17a714ff25370
Jan Starke <jan.starke@bdosecurity.de>
2024-11-05 17:50:39 +0100

    add sddl4web

---
+++ docs/sddl4web/sddl4web.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * @param {string} sddl
+ * @param {string} domain_sid
+ * @returns {string}
+ */
+export function convert(sddl: string, domain_sid: string): string;
